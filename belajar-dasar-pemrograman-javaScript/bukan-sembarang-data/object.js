b16f77249ec46ad9d665561fe116869fb2e95280
alipnf <alipnf@proton.me>
2024-09-16 14:24:13 +0700

    add new folder

---
+++ belajar-dasar-pemrograman-javaScript/bukan-sembarang-data/object.js
@@ -0,0 +1,15 @@
+const user = {
+  id: 24,
+  email: "aras@dicoding.com",
+  name: "Arsy",
+  nickname: "Aras",
+  username: "aras123",
+  password: "secret",
+};
+
+const { id, email } = user;
+console.log(id);
+console.log(email);
+
+delete user.name;
+console.log(user);
